89ecbd5808091339b143843fa54533aea2b90572
bavan <v,mbavan@kalvium.community>
2026-03-26 14:58:55 +0530

    fix: add root route

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const apiRoutes = require("./routes");
-
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1", apiRoutes);
+// root route (VERY IMPORTANT for Render test)
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Blogify API is running 🚀"
+  });
+});
+
+// routes
+const routes = require("./routes");
+app.use("/api/v1", routes);
 
 module.exports = app;
